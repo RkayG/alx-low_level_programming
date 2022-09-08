cec00e40754ec7ce9c54b690a0cb98fc5895f4ed
Your Name <you@example.com>
2022-09-08 13:29:20 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf(sizeof(float));
+	printf(sizeof(int));
+	printf(sizeof(char));
+	printf(sizeof(double));
+	return (0);
+}
+
