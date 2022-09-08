d9fb59cea05efc9fd2d269821007ba21a24a69f6
Your Name <you@example.com>
2022-09-08 14:56:54 -0700

    Assembly

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,17 @@
  */
 int main(void)
 {
-	printf(sizeof(float));
-	printf(sizeof(int));
-	printf(sizeof(char));
-	printf(sizeof(double));
+	int a;
+	char b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of an int: %lu bytes\n," (unsigned long)sizeof(a));
+	printf("Size of an char: %lu bytes\n," (unsigned long)sizeof(b));
+	printf("Size of an long int: %lu bytes\n," (unsigned long)sizeof(c));
+	printf("Size of an long long int: %lu bytes\n," (unsigned long)sizeof(d));
+	printf("Size of an float: %lu bytes\n," (unsigned long)sizeof(d));
 	return (0);
 }
 
