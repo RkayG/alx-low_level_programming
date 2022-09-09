bf655354e3e3b5b505675a48e558cfdbfba681dd
Your Name <you@example.com>
2022-09-08 21:15:50 -0700

    task 6 again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,13 @@ int main(void)
 {
 	int a;
 	char b;
-	float c;
-	double d;
+	long int c;
+	long long int d;
+	float e;
 
 	printf("Size of int: %zu bytes\n", sizeof(a));
 	printf("Size of char: %zu bytes\n", sizeof(b));
-	printf("Size of float: %zu bytes\n", sizeof(c));
-	printf("Size of double: %zu bytes\n", sizeof(d));
+	printf("Size of long int: %zu bytes\n", sizeof(c));
+	printf("Size of long long int: %zu bytes\n", sizeof(d));
+	printf("Size of float: %zu bytes\n", sizeof(e));
 }
