5566fd15d4d43b4dc6f9a2d49477639084961947
Your Name <you@example.com>
2022-09-09 12:39:35 -0700

    single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		if (a != 10 && a < 10)
+		{
+			printf("%d\n", a);
+		}
+	return (0);
+}
