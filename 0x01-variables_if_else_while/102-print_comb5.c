6291ea55e6caae9d461bbb63e0deb3fca31d945c
Your Name <you@example.com>
2022-09-12 08:27:28 -0700

    double double

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,13 +14,11 @@ int main(void)
 	{
 		for (b = a + 1; b <= 99; b++)
 		{
-			if (b > a)
-			{
-				putchar((a / 10) + '0');
-				putchar((a % 10) + '0');
-				putchar(' ');
-				putchar((a / 10) + '0');
-				putchar((b % 10) + '0');
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((a / 10) + '0');
+			putchar((b % 10) + '0');
 
 			if (a == 98 && b == 99)
 				continue;
@@ -29,9 +27,8 @@ int main(void)
 				putchar(' ');
 			}
 
-			}
-
 		}
+
 
 	}
 	putchar('\n');
