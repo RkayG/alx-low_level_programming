55e078b4d0e0126aed0b0ab26a13f239bd347f6e
Your Name <you@example.com>
2022-09-22 12:24:06 -0700

    advanced tasks

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: value if integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	/* remove last digit and recur */
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	
+	/* print last digit */
+	_putchar(n % 10 + '0');
+}
