48b2c1133774d5e1c1045385926b871c764d3235
Your Name <you@example.com>
2022-09-22 12:50:31 -0700

    magic

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,22 +8,24 @@
  */
 void print_number(int n)
 {
+	unsigned int m;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		m = -n;
 	}
 	else
 	{
-		n = n;
+		m = n;
 	}
 
 	/* remove last digit and recur */
-	if (n / 10)
+	if (m / 10)
 	{
-		print_number(n / 10);
+		print_number(m / 10);
 	}
 
 	/* print last digit */
-	_putchar(n % 10 + '0');
+	_putchar((m % 10) + '0');
 }
