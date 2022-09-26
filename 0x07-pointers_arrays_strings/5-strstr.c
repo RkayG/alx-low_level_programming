fa51ec35dbbb062e0b86b9db002ebde78204ed20
Your Name <you@example.com>
2022-09-26 13:39:47 -0700

    chess

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,17 +10,29 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i, j;
+	unsigned int i = 0, j, x;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	if (needle[0] == '\0')
+		return (haystack);
+
+	while (haystack[i] != 0)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		if (haystack[i] == needle[0])
 		{
-			if (needle[j] == haystack[i])
+			x = i, j = 0;
+			while (needle[j] != '\0')
 			{
-				return (needle);
+				if (haystack[x] == needle[j])
+					x++, j++;
+				else
+					break;
+			}
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
 			}
 		}
+		i++;
 	}
 	return (NULL);
 }
