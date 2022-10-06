7a560d830fd7bf02682b93a37c63c1965f3e7171
Your Name <you@example.com>
2022-10-06 11:25:34 -0700

    initialize with memset

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,6 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
-	/* unsigned int i; */
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
@@ -21,8 +20,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	memset(arr, 0, nmemb * size);
-	/*for (i = 0; i < nmemb * size; i++)
-		*((char *)arr + i) = 0;*/
 
 	return (arr);
 }
