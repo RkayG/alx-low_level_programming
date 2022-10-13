68b34072d5425c5a17da91cfa39993221fa356c9
Your Name <you@example.com>
2022-10-13 10:42:39 -0700

    added brackets to nil

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,16 +11,22 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
+	char *str;
+
+	if (separator == NULL)
+		return;
+
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(ap, char*));
+		str = (va_arg(ap, char*));
 
-		if (separator == NULL)
+		if (str == NULL)
 			printf("(nil)");
+		printf("%s", str);
 
-		if (i != (n - 1) && separator != NULL)
+		if (i != (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
