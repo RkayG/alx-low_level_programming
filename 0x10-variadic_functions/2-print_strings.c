2a3ff8542d0e41f1f8af419c042843b20f9c46b8
Your Name <you@example.com>
2022-10-13 10:21:55 -0700

    moved print nil statement

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,11 +18,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("%s", va_arg(ap, char*));
 
 		if (separator == NULL)
-			printf("nil");
+			printf("(nil)");
 
-		if (i == (n - 1))
-			continue;
-		printf("%s", separator);
+		if (i != (n - 1))
+			printf("%s", separator);
 	}
 	printf("\n");
 
