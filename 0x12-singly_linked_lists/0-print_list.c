a66d398e27b3d93f0298292d0f4d31c1c8e394d1
Your Name <you@example.com>
2022-10-20 13:18:19 -0700

    task 0: used if statement instead of ternary

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,14 +7,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		i++;
-		h->str ? printf("[%d] %s\n", h->len, h->str) :
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		count++;
 		h = h->next;
 	}
-	return (i);
+
+	return (count);
 }
