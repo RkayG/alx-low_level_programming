ab997f78f94efd8ccbde3c32b9842d6d55e183a0
Your Name <you@example.com>
2022-11-04 11:11:10 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0;
+	int fd, wr, i = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -25,7 +25,9 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[i])
 		i++;
 
-	write(fd, text_content, i);
+	wr = write(fd, text_content, i);
+	if (wr < 0)
+		return (-1);
 
 	close(fd);
 
